ca877ef66a128fcf295e57d93e8c9cca231d6fb1
Pedro Vella <opedro.vella@gmail.com>
2025-05-04 19:44:28 -0300

    Add comments to clarify the purpose of the keep_active.py file and enhance ping function logging

--- bot/keep_active.py
+++ bot/keep_active.py
@@ -1,10 +1,13 @@
 from flask import Flask
 import threading 
 
+### This file is used to keep the bot alive by running a Flask server that responds to ping requests.
+
 app = Flask(__name__)
 
 @app.route('/ping')
 def ping():
+    print("Ping received")
     return 'Pong!'
 
 def run():
